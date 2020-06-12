77b0fb07a2a58f12fbd56f70154b8d741540325e
Freek Jansen <freek.jansen00@hotmail.com>
2020-06-12 18:35:19 +0200

    Config.js fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+    "transpileDependencies": [
+        "vuetify"
+    ],
+    devServer: {
+        proxy: 'https://toephulp-back.smeefnet.nl/',
+    }
 }
